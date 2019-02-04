6c2c63faf238925b9071c44b676911048294cd11
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-02-04 14:56:55 +0100

    More comments inside of the code

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const app = express();
 const port = 3000;
 const fs = require('fs');
 
+/* Get the time stamp inside of the Termianl*/
+var timeStamp = new Date().toLocaleTimeString();
+
 /* The different modules */ 
-var router = require('./routes.js')
-var productRoutes = require('./product.js')
+var router = require('./router/routes.js')
+var productRoutes = require('./router/product.js')
+var usersRoutes = require('./router/users.js')
 
 /* How to access a styles... */
 app.use(express.static('public'));
 /* and pictures */
 app.use(express.static('images'));
 
+/* Logs and declare at what time you changed adress */
 var logVisits = function(req, res, next) {
     let input = new Date() + ' ' + req.path + '\n';
     fs.appendFile('accessLog', input, (err) => {
         if (err) throw err;
+        console.log("A new request recieved at: " + timeStamp);
     });
     next();
 }
@@ -28,6 +34,9 @@ app.use('/', router);
 
 /* This should only run when you seach for the products */ 
 app.use('/products', productRoutes);
+
+/* This should only run when you seach for the users */
+app.use('/users', usersRoutes); 
 
 /* Here is listen for the port and if the port is open, it will show in your Terminal */
 app.listen(port, () => {
--- router/users.js
+++ router/users.js
@@ -3,14 +3,17 @@ const express = require('express');
 var router = express.Router();
 var users = require('../users.json');
 
+/* Watches if the user has the correct ID and if it exist inside of the JSON file */
 function returnUser(uID) {
     return users.find(user => user.userID == uID);
   };
-  
+
+  /* Get the JSON when you enter the site */
   router.get("/", (req, res) => {
     res.json(users);
   });
-  
+
+  /* If they add up, it print out the ID */
   router.use("/:userID", (req, res, next) => {
     let uID = req.params.userID;
     let user = returnUser(uID);
